595548a18ee4fb71e770d9d362d88dd7b9985b0c
Lochmatter Christian <christian.lochmatter@gmx.ch>
2010-05-04 20:49:41 +0200

    added configuration file

---
+++ adressdb/cfg/config.inc.php
@@ -0,0 +1,9 @@
+<?php
+/*
+ * Created on 04.05.2010
+ *
+ * To change the template for this generated file go to
+ * Window - Preferences - PHPeclipse - PHP - Code Templates
+ */
+$g_disabled = "disabled";
+?>
